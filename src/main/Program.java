471b002892520507246ac763a46513fa95d545a2
DVMaja <majadreilinger@gmail.com>
2023-12-06 13:10:24 +0100

    Konyvtar konstruktora

---
+++ src/main/Program.java
@@ -0,0 +1,16 @@
+package main;
+
+import model.Konyvtar;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.run();
+    }
+
+    private void run() {
+        Konyvtar konyvtar = new Konyvtar();
+    }
+
+}
--- src/model/Konyvtar.java
+++ src/model/Konyvtar.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Konyvtar {
-    
+
+    private AbstractTermek[] abstraktTermekek;
+    static int abstractTermekDb;
+
+    public Konyvtar() {
+        this(5);
+    }
+
+    public Konyvtar(int db) {
+        abstraktTermekek = new AbstractTermek[db];
+        abstractTermekDb = 0;
+    }
+
 }
