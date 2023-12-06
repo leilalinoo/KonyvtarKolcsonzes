c37aa8a616b5629f94faee962f5ca14b6659509e
leilalinoo <aleila0921@gmail.com>
2023-12-06 13:32:46 +0100

    uj osztalyok, abstract kiegeszitve

--- src/model/AbstractTermek.java
+++ src/model/AbstractTermek.java
@@ -1,7 +1,49 @@
 package model;
 
+import java.util.UUID;
+
 public abstract class AbstractTermek {
     //UUID
-    //id, hossz, szerzo, cim, allapot (enum)
+    //id, hossz, szerzo, cim, allapot (enum), kidobando
+    private UUID id;
+    private String szerzo;
+    private String cim;
+    enum ALLAPOT {JOALLAPOTU, KIVALLO, ENYHENSERULT, SZAKADT};
+    private ALLAPOT allapot;
+    private boolean kidobando;
+
+    public AbstractTermek(String szerzo, String cim, ALLAPOT allapot) {
+        this.id = UUID.randomUUID();  // UUID uuid = UUID.randomUUID();
+        this.szerzo = szerzo;
+        this.cim = cim;
+        this.allapot = allapot;
+        this.kidobando = false;
+    }
+    
+    public void kuka(UUID id){
+        this.kidobando = true;
+        System.out.println("Ezt a könyvet nem lehet kikölcsönözni!");
+    }
+    
+    public void allapotValtozas(ALLAPOT ujAllapot){
+        this.allapot = ujAllapot;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    public ALLAPOT getAllapot() {
+        return allapot;
+    }
+    
     
 }
